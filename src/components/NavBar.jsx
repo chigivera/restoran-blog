5410461864cb00d7d24d5e09eeb79a46f0af17d1
chigivera <ayman.benchamkha@gmail.com>
2024-07-03 09:39:41 +0100

    good

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
             <nav className="sticky bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="hidden sm:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RestoranBlog</span>
+                        <span className="block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RestoranBlog</span>
                     </a>
                     {user ? (
                         <button 
